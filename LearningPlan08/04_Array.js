ff1184e65471508bea71772720037bd688cc4952
MicroKibaco <yangzy3@asiainfo.com>
2017-04-13 21:45:20 +0800

    数组中copyWidthin的认识
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

---
+++ LearningPlan08/04_Array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// copyWithin(position, start, end);
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// arr.copyWithin(1, 5, 7);
+
+arr.copyWithin(2, -3, 6);
+
+console.log(arr);
---
+++ LearningPlan08/05_Array.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// copyWithin(position, start, end);
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+arr.fill(10, 2, 5);
+
+console.log(arr);
