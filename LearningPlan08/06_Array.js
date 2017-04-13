02d3d28e6a3e07ce4e69ff6e554e8f56cf6d01f3
MicroKibaco <yangzy3@asiainfo.com>
2017-04-13 22:05:15 +0800

    类型化数组的认识
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

---
+++ LearningPlan08/06_Array.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let buffer = new ArrayBuffer(10);
+
+let arr = new Int8Array(buffer); // 0 - 255
+
+arr[0] = -129;
+arr[1] = 127;
+
+console.log(arr);
+
+let arr2 = new Uint16Array(buffer);
+
+console.log(arr2.length);
