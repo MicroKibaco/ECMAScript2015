1ea68609fa5e399f50c8aad1304a820aeca2c907
MicroKibaco <yangzy3@asiainfo.com>
2017-04-14 21:55:20 +0800

    遍历对象属性
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- LearningPlan09/06_iterator.js
+++ LearningPlan09/06_iterator.js
@@ -42,6 +42,7 @@ B.prototype.getClass = function () {
 
 var b = new B;
 
+console.log('-------------------------------------遍历自身可枚举属性-------------------------------------');
 // Object.keys 能够得到自身可枚举的属性,但是得不到原型链的属性
 // 得不到 Symbols 属性
 console.log(Object.keys(b));
@@ -51,6 +52,8 @@ console.log(Object.keys(b));
 // 得不到 Symbol属 性
 console.log(Object.getOwnPropertyNames(b));
 
+console.log('-------------------------------------遍历自身原型链属性-------------------------------------');
+// 可得到自身或继承原型链的属性,但必须是可枚举的属性
 console.log(Object.getOwnPropertySymbols(B.prototype));
 
 // for in 获取原型链的数据
@@ -60,3 +63,6 @@ for (let key in b) {
     console.log(key);
 
 }
+
+// 返回描述的对象
+console.log(Object.getOwnPropertyDescriptor(B.prototype, 'getAge'));
