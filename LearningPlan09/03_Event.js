e344c1e0458eeec8209770376779a29d83cbdec2
MicroKibaco <yangzy3@asiainfo.com>
2017-04-14 21:03:45 +0800

    对象属性新写法
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

---
+++ LearningPlan09/03_Event.js
@@ -0,0 +1,38 @@
+/**
+ * 对象的新属性
+ */
+
+var ok = 'ok';
+// obj['myid'+ok] = '007';
+// obj['age'+ok] = 22;
+
+obj = {
+
+    ['myid' + ok]: '007',
+    ['age' + ok]: 22,
+    getName(firstName, lastName) {
+
+        return {firstName, lastName};
+
+    },
+
+    set name(name) {
+        this.name = name;
+    },
+
+    get name() {
+
+        return this.name;
+    }
+
+};
+
+obj2 = {};
+
+// exports.obj = obj;
+// exports.obj2 = obj2;
+
+module.exports = {
+    obj, obj2
+};
+
