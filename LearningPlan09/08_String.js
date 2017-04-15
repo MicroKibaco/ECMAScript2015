b772549902b348191fa7fc01bc8ac4ee5264ad26
MicroKibaco <yangzy3@asiainfo.com>
2017-04-15 11:02:48 +0800

    字符串使用新方法
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

---
+++ LearningPlan09/08_String.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var str = '小木箱';
+
+console.log(str.indexOf('小'));
+
+console.log(str.includes('箱', 2));
+
+console.log(str.startsWith('小木', 1));
+
+console.log(str.endsWith('箱', 2));
+
+console.log(str.repeat(3));
